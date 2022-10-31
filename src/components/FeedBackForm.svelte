20b2baa5365c6a6ff244fd2b4d8c76055990db3b
KausaChangs <changayakausa@gmail.com>
2022-10-31 10:48:29 +0200

    wrapped a card around the form

--- src/components/FeedBackForm.svelte
+++ src/components/FeedBackForm.svelte
@@ -5,6 +5,11 @@
 
 </script>
 
+
+<Card>
+  <header>
+    <h2>How do you rate our services?</h2>
+  </header>
 <form>
     <!--Rating Select-->
     <div class="input-group">
@@ -12,3 +17,41 @@
         <button>Send</button>
     </div>
 </form>
+</Card>
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .message{
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
